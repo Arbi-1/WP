1b6e353da31340031e8aba78cf7ea53bd8631db6
Arbisjan Abazi <93103650+Arbi-1@users.noreply.github.com>
2023-05-02 14:44:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var button1 = document.getElementById("button1");
+button1.addEventListener("click", function() {
+  window.open("https://animixplay.fun/");
+});
+
+var button2 = document.getElementById("button2");
+button2.addEventListener("click", function() {
+  alert("This Page is under maintance. Please check later ! ");
+});
+
